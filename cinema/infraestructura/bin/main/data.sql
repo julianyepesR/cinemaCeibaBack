4c9e36097aa88b172614d0d27c11f6756d0b0db5
joseLozanoTamayo <jose.lozano.tamayo@gmail.com>
2019-02-26 14:11:25 -0500

    cambio a multiproyecto gradle

--- cinema/infraestructura/bin/main/data.sql
+++ cinema/infraestructura/bin/main/data.sql
@@ -1,14 +1,14 @@
 insert into pelicula(id, fecha_estreno, genero, titulo)
-values(1, null, 'Terror', 'El Arrogante F ');
+values(1, null, 'Terror', 'El Arrogante M ');
 
 insert into pelicula(id, fecha_estreno, genero, titulo)
-values(2, null, 'Terror', 'El Arrogante F ');
+values(2, null, 'Terror', 'El Arrogante M ');
 
 insert into pelicula(id, fecha_estreno, genero, titulo)
-values(3, null, 'Terror', 'El Arrogante F ');
+values(3, null, 'Terror', 'El Arrogante M ');
 
 insert into pelicula(id, fecha_estreno, genero, titulo)
-values(4, null, 'Terror', 'El Arrogante F ');
+values(4, null, 'Terror', 'El Arrogante M ');
 
 insert into pelicula(id, fecha_estreno, genero, titulo)
-values(5, null, 'Terror', 'El Arrogante F ');
+values(5, null, 'Terror', 'El Arrogante M ');
